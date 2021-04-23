525b0bf3e3bd60b6e5416c438f12c5e11d3b6aef
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-04-23 15:26:42 +0300

    correctly stops the log reader of there are no incoming logs

--- log-reader/dfcti_log_reader.py
+++ log-reader/dfcti_log_reader.py
@@ -19,19 +19,61 @@ class Modified_State_Handler(FileSystemEventHandler):
         if(event_path == '/private' + log_file_path):
             with open(log_file_path, 'r') as reader:
                 last_line = reader.readlines()
-                stored_values.append(last_line[len(last_line) - 1])
+                try:
+                    stored_values.append(last_line[len(last_line) - 1])
+                except Exception as error:
+                    print(f'could not write into the array')
+                    print(f'Reason -> {error}')
+                else:
+                    pass
 
 
-event_handler = Modified_State_Handler()
-observer = Observer()
-observer.schedule(event_handler, path=log_file_path, recursive=False)
+class Reader():
+    @classmethod
+    def Watch_Log_File(self, log_file, execution_time):
+
+        event_handler = Modified_State_Handler()
+
+        observer = Observer()
+        observer.schedule(event_handler, path=log_file, recursive=False)
+        observer.start()
+
+        count = 0
+        watch_state = True
+
+        total_execution_time = time.time()
+        while(watch_state):
+            # count the stack before updating it
+            current_stack_length_0 = len(stored_values)
+
+            # stop if the total execution time has passed
+            # stop if no new entries are coming into the stored values
+            if(time.time() - total_execution_time >= execution_time):
+                print(f'finished watching the log file')
+                watch_state = False
+                return
+
+            print(stored_values)
+            time.sleep(1)
+
+            # count the stack after updating it
+            current_stack_length_1 = len(stored_values)
+
+            print(stored_values)
+
+            if(current_stack_length_0 == current_stack_length_1):
+                count += 1
+            else:
+                count = 0
+            if(count == 5):  # stop if the log stream hangs up
+                print('No incoming logs...')
+                print('Stopping the watcher')
+                watch_state = False
+                return
+
+        observer.stop()
+        observer.join()
 
 
 stored_values = []
-observer.start()
-while(True):
-    time.sleep(5)
-    print(stored_values)
-    stored_values.clear()
-observer.stop()
-observer.join()
+Reader.Watch_Log_File(log_file_path, 1000)
