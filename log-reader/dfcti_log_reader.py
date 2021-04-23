845266bba548cfbd45c9124fb1a2ab9d1ed3459f
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-04-23 13:04:10 +0300

    adds skeleton script for reading logs

---
+++ log-reader/dfcti_log_reader.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+
+import os
+import platform
+import numpy as np
+from numpy import random as rd
+import time
+from datetime import datetime
+
+
+log_file_path = '/var/log/dfcti_cpu_logs.log'
