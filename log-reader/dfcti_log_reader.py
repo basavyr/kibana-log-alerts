6b62eb944e7b14cbf9a75338b5a6ec5890549514
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-04-23 14:34:51 +0300

    implemented watchdog for monitoring a certain log file. able to get the new content into dedicated array

--- log-reader/dfcti_log_reader.py
+++ log-reader/dfcti_log_reader.py
@@ -7,11 +7,31 @@ import numpy as np
 from numpy import random as rd
 import time
 from datetime import datetime
-import watchdog
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
 
 log_file_path = '/var/log/dfcti_cpu_logs.log'
 
 
-with open(log_file_path, 'r') as opener:
-    files = opener.readlines()
-    print(files)
+class Modified_State_Handler(FileSystemEventHandler):
+    def on_modified(self, event):
+        event_path = event.src_path
+        if(event_path == '/private' + log_file_path):
+            with open(log_file_path, 'r') as reader:
+                last_line = reader.readlines()
+                stored_values.append(last_line[len(last_line) - 1])
+
+
+event_handler = Modified_State_Handler()
+observer = Observer()
+observer.schedule(event_handler, path=log_file_path, recursive=False)
+
+
+stored_values = []
+observer.start()
+while(True):
+    time.sleep(5)
+    print(stored_values)
+    stored_values.clear()
+observer.stop()
+observer.join()
