345e12d84323b87881df290466f573409baee601
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-04-23 10:56:41 +0300

    add python script for writing logs

---
+++ log-writer/dfcti_log_writer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import os
+import platform
+import numpy as np
+import time
+from datetime import datetime
+
+log_file_path = '/var/log/dfcti_cpu_logs.log'
+
+MACHINE_ID = 'TEST-ID-#6969'  # this must be generated on script execution
