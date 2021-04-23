064b06ad5d2a2997d572a994e210326a8194e400
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-04-23 13:11:01 +0300

    adds watchdog package

--- log-reader/dfcti_log_reader.py
+++ log-reader/dfcti_log_reader.py
@@ -7,6 +7,11 @@ import numpy as np
 from numpy import random as rd
 import time
 from datetime import datetime
-
+import watchdog
 
 log_file_path = '/var/log/dfcti_cpu_logs.log'
+
+
+with open(log_file_path, 'r') as opener:
+    files = opener.readlines()
+    print(files)
