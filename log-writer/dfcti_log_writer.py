5c0b3b6a837bba0f50407ce9d7a4b1279e35b9ba
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-04-23 11:09:57 +0300

    implemented stats for CPU and RAM in the writer

--- log-writer/dfcti_log_writer.py
+++ log-writer/dfcti_log_writer.py
@@ -1,11 +1,53 @@
 #!/usr/bin/env python3
 
+
 import os
 import platform
 import numpy as np
+from numpy import random as rd
 import time
 from datetime import datetime
 
-log_file_path = '/var/log/dfcti_cpu_logs.log'
 
+log_file_path = '/var/log/dfcti_cpu_logs.log'
 MACHINE_ID = 'TEST-ID-#6969'  # this must be generated on script execution
+
+
+class SystemLogs:
+    """Generates any stats related to system logs.
+    For example, it can generate CPU usage, disk usage, memory (RAM) usage, and also network usage.
+    The generated stats are usually percentages, representing how much of the total allocated resources of that particular machine are being used.
+    """
+    @classmethod
+    def CPU(self, mean_cpu_usage, usage_spread):
+        """Gives the current CPU usage
+        This is based on a normal distribution, centered around `mean_cpu_usage`
+        The returned values are spreading along accordint to the size of the standard deviation `usage_spread`
+        """
+        # this is set by the user when running the script
+        MEAN_CPU_USAGE = mean_cpu_usage
+        # the standard deviation is set by the user when running the script
+        STD_DEV = usage_spread
+
+        cpu_usage = lambda: round(rd.normal(MEAN_CPU_USAGE, STD_DEV), 2)
+        instant_usage = abs(cpu_usage())
+        while(instant_usage > 100):
+            instant_usage = abs(cpu_usage())
+        return instant_usage
+
+    @classmethod
+    def MEM(self, mean_disk_usage, usage_spread):
+        """Gives the current DISK usage
+        This is based on a normal distribution, centered around `mean_disk_usage`
+        The returned values are spreading along accordint to the size of the standard deviation `usage_spread`
+        """
+        # this is set by the user when running the script
+        MEAN_MEM_USAGE = mean_disk_usage
+        # the standard deviation is set by the user when running the script
+        STD_DEV = usage_spread
+
+        memory_usage = lambda: round(rd.normal(MEAN_MEM_USAGE, STD_DEV), 2)
+        instant_usage = abs(memory_usage())
+        while(instant_usage > 100):
+            instant_usage = abs(memory_usage())
+        return instant_usage
