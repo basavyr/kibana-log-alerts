2b696c9a3a79bfa0e72ab6777cd841e575f51981
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-05-05 18:48:25 +0300

    add unit test for writing log lines

--- log-writer/test_writer.py
+++ log-writer/test_writer.py
@@ -28,10 +28,27 @@ class Test(unittest.TestCase):
             self.logs.append(line)
             time.sleep(logwriter.REFRESH_CYCLE)
         end_time = time.time()
-        self.assertEqual(len(self.logs),n_iterations)
+        self.assertEqual(len(self.logs), n_iterations)
         self.assertGreaterEqual(end_time - start_time,
                                 logwriter.REFRESH_CYCLE * n_iterations)
         print(f'Finished the test for generating system stats')
+
+    def test_Write_Log_Line(self):
+        print(f'Starting the test for writing logs')
+        n_iterations = 10
+        log_file_path = logwriter.log_file_path
+        log_lines = []
+
+        for idx in range(n_iterations):
+            line = self.writer.Generate_System_Log_Line()
+            self.assertIsNotNone(line)
+            log_lines.append(line)
+            self.assertGreaterEqual(len(log_lines), 0)
+
+        for line in log_lines:
+            yy_liner = self.writer.Write_Log_Line(line, log_file_path)
+            self.assertEqual(yy_liner, 1)
+        print(f'Finished the test for writing logs')
 
 
 if __name__ == '__main__':
