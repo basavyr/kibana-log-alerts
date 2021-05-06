7d62e0dc791e24c18beaaf7b1ada80cc60ba01b1
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-05-06 11:03:26 +0300

    removed docker cp command

--- scripts/tests_entrypoint.sh
+++ scripts/tests_entrypoint.sh
@@ -8,11 +8,11 @@ pyenv local systems
 python -m pip install --upgrade pip
 pip install pipenv
 pipenv install
-pipenv run python dfcti_log_reader.py 11 5
+# pipenv run python dfcti_log_reader.py 11 5
 pipenv run python test_reader.py
 cd ../writer/
 pyenv local systems
 pipenv install
-pipenv run python dfcti_log_writer.py 10
+#pipenv run python dfcti_log_writer.py 10
 pipenv run python test_writer.py
 cat /var/log/dfcti_system_logs.log
