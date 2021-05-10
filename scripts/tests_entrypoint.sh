44c174cc748fadce7503fdbbdf54be4baa53c01c
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-05-10 14:07:53 +0300

    moves docker unit testing to separate workflow

--- scripts/tests_entrypoint.sh
+++ scripts/tests_entrypoint.sh
@@ -8,13 +8,11 @@ pyenv local systems
 python -m pip install --upgrade pip
 pip install pipenv
 pipenv install
-# pipenv run python dfcti_log_reader.py 11 5
 echo 'Run the reader test'
 pipenv run python test_reader.py
 cd ../writer/
 pyenv local systems
 pipenv install
-#pipenv run python dfcti_log_writer.py 10
 echo 'Run the writer test'
 pipenv run python test_writer.py
 cat /var/log/dfcti_system_logs.log
