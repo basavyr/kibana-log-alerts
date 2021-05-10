13cef34f2c19222b9505b108defe1e03dd8450e3
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-05-10 10:57:20 +0300

    improves scripting

---
+++ scripts/logstash_pipeline.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+cycle_time=$2
+
+node_name=$(uname -n)
+elk_node_name='elk.nipne.ro'
+
+virtualenvName=systems
+
+log_file_path=/var/log/dfcti_system_logs.log
+
+if [ $node_name == $elk_node_name ];
+then
+    echo 'Creating the log file....'
+    touch $log_file_path
+    chmod 777 $log_file_path
+    cd ../log-reader/
+    echo 'Reader -> Activating a virtual environment and installing packages from the Pipfile...'
+    pyenv virtualenv $virtualenvName
+    pyenv local $virtualenvName
+    python -m pip install --upgrade pip
+    python -m pip install pipenv
+    pipenv install
+    echo 'Writer -> Activating a virtual environment and installing packages from the Pipfile...'
+    cd ../log-writer/
+    pyenv local $virtualenvName
+    pipenv install
+    nohup pipenv run python dfcti_log_writer.py $1 &
+    cd ../log-reader/
+    pipenv run python dfcti_log_reader.py $2
+else
+    echo Cannot run the script on this node.
+fi
+ 
+
+
